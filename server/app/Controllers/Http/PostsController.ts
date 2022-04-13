d23154a7d1baf4fdb48cded75bbf1a04572f2058
brojor <bronislav.jordan@gmail.com>
2022-04-13 19:58:21 +0200

    feat: add route to adding new posts

--- server/app/Controllers/Http/PostsController.ts
+++ server/app/Controllers/Http/PostsController.ts
@@ -1,3 +1,12 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
-export default class PostsController {}
+export default class PostsController {
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.body()
+    const post = Post.create(body)
+
+    response.status(201)
+    return post
+  }
+}
