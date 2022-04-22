de333f5c60783587a35065d8c6fa5416e7b8a733
brojor <bronislav.jordan@gmail.com>
2022-04-22 18:43:19 +0200

    feat: add main navbar to left aside element

---
+++ client/src/components/MainNav.vue
@@ -0,0 +1,120 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li v-for="navItem in mainNavItems" :key="`nav-item-${navItem.id}`">
+        <a href="">
+          <component :is="navItem.component" class="nav-icon" />
+          {{ navItem.name }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import NavHome from './icons/nav/NavHome.vue';
+import NavReadingList from './icons/nav/NavReadingList.vue';
+import NavListings from './icons/nav/NavListings.vue';
+import NavPodcasts from './icons/nav/NavPodcasts.vue';
+import NavVideos from './icons/nav/NavVideos.vue';
+import NavTags from './icons/nav/NavTags.vue';
+import NavFAQ from './icons/nav/NavFAQ.vue';
+import NavForemShop from './icons/nav/NavForemShop.vue';
+import NavSponsors from './icons/nav/NavSponsors.vue';
+import NavAbout from './icons/nav/NavAbout.vue';
+import NavContact from './icons/nav/NavContact.vue';
+
+const mainNavItems = [
+  {
+    id: 0,
+    name: 'Home',
+    component: NavHome,
+  },
+  {
+    id: 1,
+    name: 'Reading List',
+    component: NavReadingList,
+  },
+  {
+    id: 2,
+    name: 'Listings',
+    component: NavListings,
+  },
+  {
+    id: 3,
+    name: 'Podcasts',
+    component: NavPodcasts,
+  },
+  {
+    id: 4,
+    name: 'Videos',
+    component: NavVideos,
+  },
+  {
+    id: 5,
+    name: 'Tags',
+    component: NavTags,
+  },
+  {
+    id: 6,
+    name: 'FAQ',
+    component: NavFAQ,
+  },
+  {
+    id: 7,
+    name: 'Forem Shop',
+    component: NavForemShop,
+  },
+  {
+    id: 8,
+    name: 'Sponsors',
+    component: NavSponsors,
+  },
+  {
+    id: 9,
+    name: 'About',
+    component: NavAbout,
+  },
+  {
+    id: 10,
+    name: 'Contact',
+    component: NavContact,
+  },
+];
+</script>
+
+<style scoped>
+ul {
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: rgb(212, 212, 212);
+  display: flex;
+  padding: 0.5rem 1rem;
+
+  border-radius: var(--radius);
+  font-size: 1rem;
+  cursor: pointer;
+  outline: 0;
+  line-height: 1.5rem;
+  /* max-width: 100%; */
+}
+a:hover {
+  color: rgb(165, 180, 252);
+  text-decoration: underline;
+  background-color: rgba(49, 46, 129, 0.75);
+  border-radius: 0.375rem;
+}
+
+.nav-icon {
+  margin-right: 0.5rem;
+  margin-left: -0.5rem;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,6 +4,8 @@ import PostComponent from '../components/PostComponent.vue';
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
 
+import MainNav from '../components/MainNav.vue';
+
 const posts = ref<any>([]);
 
 onMounted(async () => {
@@ -11,35 +13,14 @@ onMounted(async () => {
   posts.value = data;
 });
 
-// const posts = ref([
-//   {
-//     id: 1,
-//     title: 'Hello, World!',
-//     user: {
-//       name: 'phongdang707',
-//       avatar:
-//         'https://res.cloudinary.com/practicaldev/image/fetch/s--zqc1Dc7d--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/381638/ebbbc99b-48a6-4dd6-ba3c-90698d3b9a38.jpg',
-//     },
-//     publishDate: new Date('2022/04/15'),
-//     tags: ['discuss', 'help'],
-//   },
-//   {
-//     id: 2,
-//     title: 'Landing page',
-//     user: {
-//       name: 'Umar Deepanshu',
-//       avatar:
-//         'https://res.cloudinary.com/practicaldev/image/fetch/s--1oN03ZGR--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/547736/36cfad3b-d1ea-4da8-bfcb-edb46455a374.jpeg',
-//     },
-//     publishDate: new Date('2022/04/16'),
-//     tags: ['javascript', 'nextjs', 'webdev'],
-//   },
-// ]);
+
 </script>
 
 <template>
   <div class="index-container">
-    <aside class="sidebar-left"></aside>
+    <aside class="sidebar-left">
+      <MainNav />
+    </aside>
     <main>
       <PostComponent v-for="post in posts" :key="post.id" v-bind="post" />
     </main>
@@ -63,7 +44,7 @@ main {
 }
 
 .sidebar-left {
-  border: 1px solid sandybrown;
+  /* border: 1px solid sandybrown; */
 }
 
 .sidebar-right {
